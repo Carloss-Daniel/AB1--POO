75478c030386d1cde6b40d91a7ec1b7ee1f30f7d
Carlos Daniel <65622154+EuDanzin1@users.noreply.github.com>
2022-10-13 10:02:06 -0300

    Create GoogleProvider.java

---
+++ Questão-3/adapters/GoogleProvider.java
@@ -0,0 +1,13 @@
+package adapters;
+
+import java.util.Optional;
+
+import entities.User;
+
+public class GoogleProvider {
+
+    public User sign(Optional<Object> credentials) {
+        return null;
+    }
+
+}
