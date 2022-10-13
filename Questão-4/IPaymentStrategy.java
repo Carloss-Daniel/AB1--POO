7cdc63cec4be3ad6977625197fbc192930e8ec5f
Carlos Daniel <65622154+EuDanzin1@users.noreply.github.com>
2022-10-13 11:32:25 -0300

    Create IPaymentStrategy.java

---
+++ Questão-4/IPaymentStrategy.java
@@ -0,0 +1,7 @@
+
+public interface IPaymentStrategy {
+    // nos parâmentros da função você pode defenir uma entrada padrão ou usar o generics do java
+    // Aqui vou deixar sem nada
+    public void payment ();
+  }
+  
